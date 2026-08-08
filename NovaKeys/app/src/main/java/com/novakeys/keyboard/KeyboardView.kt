8c384e7d2cece5be89c7deff9080936f3b2ba146
bezawitbiruk01-gif <bezawitbiruk01@gmail.com>
2026-08-08 06:56:24 +0300

    Fix unresolved Compose awaitPointerEvent import

--- NovaKeys/app/src/main/java/com/novakeys/keyboard/KeyboardView.kt
+++ NovaKeys/app/src/main/java/com/novakeys/keyboard/KeyboardView.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.awaitPointerEvent
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
